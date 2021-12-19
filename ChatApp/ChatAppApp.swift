ce2461f6a0aa783be3659b31e88de722f643a92d
omerkrsvr <omerkirsever@gmail.com>
2021-12-19 20:28:10 +0300

    Initial Commit

---
+++ ChatApp/ChatAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatAppApp.swift
+//  ChatApp
+//
+//  Created by Omer Kırsever on 19.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct ChatAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
